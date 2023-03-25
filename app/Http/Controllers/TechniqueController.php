6500bf9ebfed6aa622c2cb00e80a5ee8f2f6682c
Adam Haley <adamhaley@gmail.com>
2023-03-24 19:08:46 -0700

    removed json middleware, added Session and Technique Resource controllers

--- app/Http/Controllers/TechniqueController.php
+++ app/Http/Controllers/TechniqueController.php
@@ -2,9 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Technique;
 use Illuminate\Http\Request;
 
 class TechniqueController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //set json headers
+        //return json of the techniques
+        $data = Technique::with('belt')->get();
+
+        return response()->json($data);
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Technique $technique)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Technique $technique)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Technique $technique)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Technique $technique)
+    {
+        //
+    }
 }
